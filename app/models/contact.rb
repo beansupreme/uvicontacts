f9ddeb5bc7a98aefa2ced1175794b60cdf7b5568
Andy Whillans <andy.whillans@gmail.com>
2018-04-15 18:15:26 -0400

    Tweak csv import code to prevent importing the header row as a contact

--- app/models/contact.rb
+++ app/models/contact.rb
@@ -2,16 +2,24 @@ class Contact < ActiveRecord::Base
   require 'csv'
 
   def self.import(file)
+    index = 0
     CSV.foreach(file.path, headers: true) do |row|
-      puts row.header_row?
-      if row[0].present?
-        Contact.create!(
-          name: row[0], 
-          telephone: row[1], 
-          emergency_contact_name: row[2],
-          emergency_contact_telephone: row[3]
-        )
-      end
+      index += 1
+      next if index == 1
+      create_contact_from_csv_row(row)
+    end
+  end
+
+  private
+
+  def self.create_contact_from_csv_row(row)
+    if row[0].present?
+      Contact.create!(
+        name: row[0], 
+        telephone: row[1], 
+        emergency_contact_name: row[2],
+        emergency_contact_telephone: row[3]
+      )
     end
   end
 end
--- spec/models/contact_spec.rb
+++ spec/models/contact_spec.rb
@@ -1,5 +1,42 @@
 require 'rails_helper'
 
 RSpec.describe Contact, type: :model do
-  pending "add some examples to (or delete) #{__FILE__}"
+  
+  describe '.import' do
+    let(:file_to_upload) do
+      Rack::Test::UploadedFile.new(contact_file_path, 'text/csv')
+    end
+    
+    let(:contact_file_path) do
+      Rails.root.join('spec', 'support', contacts_list_file_name)
+    end
+
+    context 'with a csv that has a header' do
+      context 'and multiple valid rows' do
+        let(:contacts_list_file_name) do
+          'contacts_list_with_five_records_and_header.csv'
+        end
+
+        it 'creates contacts for each record in the csv, and skips the header' do
+          expect(Contact.count).to eq(0)
+
+          Contact.import(file_to_upload)
+
+          expect(Contact.count).to eq(5)
+        end
+      end
+
+      context 'and is empty' do
+        let(:contacts_list_file_name) do
+          'invalid_contacts_list.csv'
+        end
+
+        it 'does not create any contacts' do
+          expect{
+            Contact.import(file_to_upload)
+          }.not_to change(Contact, :count)
+        end
+      end
+    end
+  end
 end
